1e379af1ef626b969155302c57cd555285cb212f
Dan Cristian Oancea <oanceadancristian@gmail.com>
2024-01-31 14:28:32 +0200

    Layout changes 10

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{' '}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            OnlyFans Agency
+            Allure Guardian Agency
           </a>{' '}
         </div>
 
