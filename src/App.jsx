9e44af57a6d22bdc470fe3e0dbf0f26b43c3515b
Dan Cristian Oancea <oanceadancristian@gmail.com>
2024-02-11 21:34:00 +0200

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { Services } from './components/services';
 import { Faqs } from './components/faqs';
 import { Booking } from './components/booking';
 import { Contact } from './components/contact';
+import { NotFound } from './components/notFound';
 import JsonData from './data/data.json';
 import SmoothScroll from 'smooth-scroll';
 import './App.css';
@@ -24,20 +25,48 @@ const App = () => {
     setLandingPageData(JsonData);
   }, []);
 
-  return (
-    <div>
-      <Navigation />
-      <Header data={landingPageData.Header} />
-      <Features data={landingPageData.Features} />
-      <About data={landingPageData.About} />
-      <Services data={landingPageData.Services} />
-      {/* <Gallery data={landingPageData.Gallery} /> */}
-      {/* <Testimonials data={landingPageData.Testimonials} /> */}
-      <Faqs data={landingPageData.Faqs} />
-      <Booking />
-      <Contact data={landingPageData.Contact} />
-    </div>
-  );
+  const url = window.location.href;
+  console.log(url);
+
+  const myUrlList = [
+    'http://localhost:3000/',
+    'http://localhost:3000/#page-top',
+    'http://localhost:3000/#features',
+    'http://localhost:3000/#about',
+    'http://localhost:3000/#services',
+    'http://localhost:3000/#faqs',
+    'http://localhost:3000/#booking',
+    'http://localhost:3000/#contact',
+    'allureguardianagency.com/',
+    'allureguardianagency.com/#page-top',
+    'allureguardianagency.com/#features',
+    'allureguardianagency.com/#about',
+    'allureguardianagency.com/#services',
+    'allureguardianagency.com/#faqs',
+    'allureguardianagency.com/#booking',
+    'allureguardianagency.com/#contact',
+  ];
+
+  const exists = myUrlList.includes(url);
+  console.log(exists);
+
+  if (exists) {
+    return (
+      <div>
+        <Navigation />
+        <Header data={landingPageData.Header} />
+        <Features data={landingPageData.Features} />
+        <About data={landingPageData.About} />
+        <Services data={landingPageData.Services} />
+        {/* <Gallery data={landingPageData.Gallery} /> */}
+        {/* <Testimonials data={landingPageData.Testimonials} /> */}
+        <Faqs data={landingPageData.Faqs} />
+        <Booking />
+        <Contact data={landingPageData.Contact} />
+      </div>
+    );
+  }
+  return <NotFound data={landingPageData.NotFound} />;
 };
 
 export default App;
---
+++ src/components/notFound.jsx
@@ -0,0 +1,12 @@
+export const NotFound = (props) => {
+  return (
+    <div className="container text-center" style={{ marginTop: '250px' }}>
+      <h1 style={{ marginBottom: '25px' }}>
+        {props.data ? props.data.message : 'Loading'}
+      </h1>
+      <a href="http://localhost:3000/">
+        <button className="btn-custom">Go back home</button>
+      </a>
+    </div>
+  );
+};
