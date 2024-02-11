ebe0c3e4cfd55d4d5b25febda40bff423aa2d8a8
Dan Cristian Oancea <oanceadancristian@gmail.com>
2024-02-11 22:41:04 +0200

    Changes

--- src/components/notFound.jsx
+++ src/components/notFound.jsx
@@ -4,7 +4,7 @@ export const NotFound = (props) => {
       <h1 style={{ marginBottom: '25px' }}>
         {props.data ? props.data.message : 'Loading'}
       </h1>
-      <a href="http://localhost:3000/">
+      <a href="/">
         <button className="btn-custom">Go back home</button>
       </a>
     </div>
