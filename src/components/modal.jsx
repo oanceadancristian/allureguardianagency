68bc9d64fef4ed82a18e1aa624d6c5dfa9fc4491
Dan Cristian Oancea <oanceadancristian@gmail.com>
2024-04-22 20:24:39 +0300

    Gallery

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const modal = {
+const myModal = {
   position: 'fixed',
   zIndex: 1,
   left: 0,
@@ -11,17 +11,19 @@ const modal = {
   backgroundColor: 'rgba(0, 0, 0, 0.8)',
 };
 
-const close = {
-  position: 'absolute',
-  top: 70,
-  right: 350,
-  color: '#f1f1f1',
-  fontSize: 50,
-  fontWeight: 'bold',
-  cursor: 'pointer',
+const myModalClose = {
+  container: (isLaptop) => ({
+    position: 'absolute',
+    top: isLaptop ? '70' : '185',
+    right: isLaptop ? '350' : '150',
+    color: '#f1f1f1',
+    fontSize: 50,
+    fontWeight: 'bold',
+    cursor: 'pointer',
+  }),
 };
 
-const modalContent = {
+const myModalContent = {
   display: 'flex',
   alignItems: 'center',
   width: '50%',
@@ -34,12 +36,21 @@ export const Modal = ({ onOpen, children }) => {
 };
 
 export const ModalContent = ({ onClose, children }) => {
+  const mediaMatch = window.matchMedia('(min-width: 768px)');
+  const [matches, setMatches] = useState(mediaMatch.matches);
+
+  useEffect(() => {
+    const handler = (e) => setMatches(e.matches);
+    mediaMatch.addListener(handler);
+    return () => mediaMatch.removeListener(handler);
+  });
+
   return (
-    <div style={modal}>
-      <span style={close} onClick={onClose}>
+    <div style={myModal}>
+      <span style={myModalClose.container(matches)} onClick={onClose}>
         &times;
       </span>
-      <div style={modalContent}>{children}</div>
+      <div style={myModalContent}>{children}</div>
     </div>
   );
 };
